a3d2c8d1a74a53ef4511fed698d9e932d3e9181a
Junaidahmed009 <jr124880@gmail.com>
2026-02-06 22:04:45 +0500

    testing if server is working after deploy.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req,res)=>{
-    return res.json({msg: 'Hello from the server'})
+    return res.json({msg: 'Hello from the server  ooh paa ji bari dair lagi ja actions chlaunde chlaunde'})
 })
 
 app.listen(PORT,()=>{
